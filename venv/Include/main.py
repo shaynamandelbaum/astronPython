836a9df7d9885cadcae09be5970bd85646c3ed99
Shayna Mandelbaum <shayna.mandelbaum@gmail.com>
2020-12-29 01:07:19 -0500

    initial commit

---
+++ venv/Include/main.py
@@ -0,0 +1,15 @@
+import pygame
+map = pygame.image.load('C:\\Users\\shayn\\PycharmProjects\\astron\\venv\\Include\\astronMap.png')
+(width,height) = (600,600)
+screen = pygame.display.set_mode((width,height))
+2481,554
+map = pygame.transform.smoothscale(map,(1860,413))
+
+screen.blit(map,(0,0))
+pygame.display.flip()
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+      pygame.quit()
