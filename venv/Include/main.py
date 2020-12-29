0c14f6d7f8de42d119247dc802b1e02baef9f291
Shayna Mandelbaum <shayna.mandelbaum@gmail.com>
2020-12-29 01:21:19 -0500

    fixing imaging, moving onto model

--- venv/Include/main.py
+++ venv/Include/main.py
@@ -2,10 +2,10 @@ import pygame
 map = pygame.image.load('C:\\Users\\shayn\\PycharmProjects\\astron\\venv\\Include\\astronMap.png')
 (width,height) = (600,600)
 screen = pygame.display.set_mode((width,height))
-2481,554
+#2481,554
 map = pygame.transform.smoothscale(map,(1860,413))
 
-screen.blit(map,(0,0))
+screen.blit(map,(100,100),(300,50,600,300))
 pygame.display.flip()
 running = True
 while running:
